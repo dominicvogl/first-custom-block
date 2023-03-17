7fa3f9a210aaad84749fd61d1dc4021bc3e8ae1f
Dominic Vogl <dominic.vogl@cat-ia.de>
2023-03-17 10:53:19 +0100

    use react components to render in editor and frontend

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var registerBlockType = wp.blocks.registerBlockType;
+var createElement = wp.element.createElement;
 
 registerBlockType("dominic-vogl/firstblock", {
 	edit: function () {
-		return "Edit";
+		return createElement(
+			"p",
+			{
+				className: "my-first-block",
+			},
+			"Hello World, step 1"
+		);
 	},
 	save: function () {
-		return "Save";
+		return createElement("p", null, "Save World, step 1");
 	},
 });
