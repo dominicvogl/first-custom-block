ccb5b81fe3bee8b187857f59b946870c7f246148
Dominic Vogl <dominic.vogl@cat-ia.de>
2023-03-17 11:37:24 +0100

    add scss compiler for styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
+// Register the block, Function from Wordpress Version of react
 import { registerBlockType } from "@wordpress/blocks";
+// use the blockprops for the attributes on the html element for the editor and the frontend (classes, title, etc. Attributes)
+import { useBlockProps } from "@wordpress/block-editor";
+import "./style.scss";
+import "./editor-style.scss";
 
+// register the block
 registerBlockType("dominic-vogl/firstblock", {
 	edit: function () {
-		return <p className="myfirstBlock">Edit this Stuff!</p>;
+		return <p {...useBlockProps()}>Edit this Stuff!</p>;
 	},
 	save: function () {
-		return <p className="myfirstBlock">Save JSX this Stuff!</p>;
+		return <p {...useBlockProps.save()}>Save JSX this Stuff!</p>;
 	},
 });
